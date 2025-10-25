fcb9b31be785718abe3a221b7c2ff8d44d63c271
vinay patil <vinaypatil135.vp@gmail.com>
2025-10-26 00:19:05 +0530

    console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter no");
+int num = Convert.ToInt32(Console.ReadLine()),sum = 0, temp = num;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += digit * digit * digit; // For 3-digit numbers
+            temp /= 10;
+        }
+
+        if (sum == num)
+            Console.WriteLine($"{num} is an Armstrong number.");
+        else
+            Console.WriteLine($"{num} is not an Armstrong number.");
+
